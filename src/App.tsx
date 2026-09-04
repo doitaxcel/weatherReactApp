8e156b10527eaf5591c5dfebbaff2f92f38b4e6d
Axcel Guevarra <axcelbattleground@gmail.com>
2026-09-04 19:40:12 +0800

    fix: Padding

--- src/App.tsx
+++ src/App.tsx
@@ -13,30 +13,32 @@ function App() {
 
   return (
     <>
-      <header className='flex justify-between m-6 gap-3 md:gap-5'>
-        <Header />
-        <SearchBar />
-        <ToggleButton />
-      </header>
-      <main>
-        <section>
-          <Summary />
-        </section>
+      <div className='max-w-5xl mx-auto'>
+        <header className='flex justify-between m-6 gap-3 md:gap-5'>
+          <Header />
+          <SearchBar />
+          <ToggleButton />
+        </header>
+        <main>
+          <section>
+            <Summary />
+          </section>
 
-        <section className='grid grid-cols-2 lg:grid-cols-3 m-6 gap-4'>
-          <Card Title="Humidity" Data="65%" Logo={Droplets} />
-          <Card Title="Wind" Data="65%" Logo={Wind} />
-          <Card Title="Pressure" Data="65%" Logo={Gauge} />
-          <Card Title="Visibility" Data="65%" Logo={Eye} />
-          <Card Title="UV Index" Data="HIGH" Logo={Sun} />
-          <Card Title="Chance of Rain" Data="65%" Logo={CloudHail} />
-        </section>
+          <section className='grid grid-cols-2 lg:grid-cols-3 m-6 gap-4'>
+            <Card Title="Humidity" Data="65%" Logo={Droplets} />
+            <Card Title="Wind" Data="65%" Logo={Wind} />
+            <Card Title="Pressure" Data="65%" Logo={Gauge} />
+            <Card Title="Visibility" Data="65%" Logo={Eye} />
+            <Card Title="UV Index" Data="HIGH" Logo={Sun} />
+            <Card Title="Chance of Rain" Data="65%" Logo={CloudHail} />
+          </section>
 
-        <section>
-          <HourlyForecast />
-        </section>
+          <section>
+            <HourlyForecast />
+          </section>
 
-      </main>
+        </main>
+      </div>
     </>
   )
 }
