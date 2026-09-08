3d504520a4780ee1a6f31be61e2d0207fa77d422
Axcel Guevarra <axcelbattleground@gmail.com>
2026-09-08 17:11:27 +0800

    feat: cursor automatically on searchBar at first load

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,4 +1,5 @@
 import { Search } from "lucide-react";
+import { useEffect, useRef } from "react";
 
 interface Props {
     onSearch: (city: string) => void;
@@ -6,19 +7,28 @@ interface Props {
 }
 
 const SearchBar = ({ onSearch, error }: Props) => {
+    const inputElement = useRef<HTMLInputElement | null>(null);
+
+    useEffect(() => {
+        inputElement.current?.focus();
+    }, []);
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        const form = e.currentTarget;
+        const input = form.elements.namedItem("city") as HTMLInputElement;
+
+        if (!input.value.trim()) return;
+
+        onSearch(input.value.trim());
+        input.value = "";
+        inputElement.current?.focus();
+    }
+    
     return (
         <form
-            onSubmit={(e) => {
-                e.preventDefault();
-
-                const form = e.currentTarget;
-                const input = form.elements.namedItem("city") as HTMLInputElement;
-
-                if (!input.value.trim()) return;
-
-                onSearch(input.value.trim());
-                input.value = "";
-            }}
+            onSubmit={handleSubmit}
             className="flex items-center w-full max-w-md"
         >
             <div className="flex relative items-center w-full bg-white rounded-2xl border border-gray-200 shadow-sm px-4 py-2 transition-all duration-200 focus-within:border-blue-400 focus-within:ring-4 focus-within:ring-blue-100">
@@ -29,6 +39,7 @@ const SearchBar = ({ onSearch, error }: Props) => {
                 />
 
                 <input
+                    ref={inputElement}
                     name="city"
                     type="text"
                     placeholder="Search city..."
